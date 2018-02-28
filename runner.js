ca8bf9b644165215aab3028eb0d8d4c010bcf625
s.pahmurny <s.pahmurny@gmail.com>
2018-02-28 09:38:52 +0300

    removed unused code

--- runner.js
+++ runner.js
@@ -3,31 +3,6 @@ const async = require('async');
 const newman = require('newman');
 const axios = require('axios');
 
-// const parametersForTestRun = {
-//   collection: path.join(__dirname, 'postman_collection.json'), // your collection
-//   environment: path.join(__dirname, 'postman_environment.json'), //your env
-// };
-// console.log(parametersForTestRun);
-// const parallelCollectionRun = (done) => {
-//   newman.run(parametersForTestRun, done);
-// };
-
-// const times = process.argv[2] ? +process.argv[2] : 1;
-// console.log('Running request ', times,' times');
-/*
-let runArray = Array(times).fill(parallelCollectionRun);
-
-async.parallel(runArray,
-  (err, results) => {
-    err && console.error(err);
-    results.forEach((result) => {
-      let failures = result.run.failures;
-      // console.log(result.run.executions[0].response);
-      console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
-        `${result.collection.name} ran successfully.`);
-    });
-  });
-  */
 const times = process.argv[2] ? +process.argv[2] : 1;
 const url = 'https://touchcr-api-dev.herokuapp.com/api/v1/orders/createguest';
 const productPrice = 106;
@@ -74,8 +49,6 @@ for (let i = 0; i < times; i++){
   newBody.orderTotal = (i+1) * productPrice;
   newBody.accDetails.email = `stresstesterapi${i}@gmail.com`;
   const parallelCollectionRun = (done) => {
-    // console.log('url', url);
-    // console.log('body', newBody);
     return axios.post(url, newBody, {  })
       .then(res => console.log('Axios success: ', i))
       .catch((error) => console.log('Axios error', error.message) );
@@ -85,16 +58,5 @@ for (let i = 0; i < times; i++){
 
 async.parallel(reqArray,
   (err, results) => {
-    // console.log(results);
     err && console.error(err);
-    results.forEach((result) => {
-      console.log(result);
-      // let failures = result.run.failures;
-      // console.log(result.run.executions[0].response);
-      // console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
-      //   `${result.collection.name} ran successfully.`);
-    });
   });
-  // .catch((error) => {
-  //   console.log('Paralel error', error.message);
-  // })
